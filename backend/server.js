a9f8ed4fbb93c648c05e351254c11a1a560b7141
aqilsyahmi <90232209+aqilsyahmi@users.noreply.github.com>
2023-11-04 16:01:32 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser")
 const app = express();
 require('dotenv').config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/authRoute");
 const cartRoute = require("./routes/cartRoute");
@@ -15,18 +15,20 @@ const categoryRoute = require("./routes/categoryRoute");
 const filterRoute = require("./routes/filterRoute");
 const {notFound} = require('./middlewares/errorHandler')
 
+const corsOptions = {
+  origin: 'http://54.169.168.70:3000', // Replace with the URL of your frontend
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies and authorization headers
+};
+
 //middleware
 // app.use(cors()); //all requests from all origins
-app.use((req, res, next) => {
-res.setHeader("Access-Control-Allow-Origin", "http://54.169.168.70:3000") // Replace with your frontend's URL
-res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
-next()
-})
+app.use(cors(corsOptions));
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/api/auth", authRoute);
 app.use('/api/cart',cartRoute);
@@ -34,19 +36,36 @@ app.use('/api/products', productRoute);
 app.use('/api/categories', categoryRoute);
 app.use('/uploads', express.static('uploads'));
 app.use('/api/filter', filterRoute);
+
+app.get('/', (req, res) => {
+    res.json({ message: "Backend server is running!" });
+});
+
 app.use(notFound)
-//start the server
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+res.setHeader("Access-Control-Allow-Origin", "http://54.169.168.70:3000") // Replace with your frontend's URL
+res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+next()
+})
+
+
+// Connect to DB and then start the server
 const connectDB = require("./database/db");
 
-app.listen(PORT, () => {console.log(`Server is running at PORT ${PORT}`);});
-connectDB();
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running at PORT ${PORT}`);
+    });
+}).catch((error) => {
+    console.error("Failed to connect to DB:", error);
+    process.exit(1);
+});
 
-const corsOptions = {
-origin: 'http://54.169.168.70:3000', // Replace with the URL of your frontend
-methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-credentials: true, // Allow cookies and authorization headers
-};
 
-app.use(cors(corsOptions));
 
-app.use(cookieParser());
+
+
+
