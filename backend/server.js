19525f3adcc11fe093bc5a30d179d432a10b1d44
aqilsyahmi <90232209+aqilsyahmi@users.noreply.github.com>
2023-11-04 16:37:37 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,11 @@ const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
 const app = express();
 require('dotenv').config();
 
 const PORT = process.env.PORT || 4000;
-
+const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/authRoute");
 const cartRoute = require("./routes/cartRoute");
 const productRoute = require("./routes/productRoute");
@@ -16,20 +14,21 @@ const categoryRoute = require("./routes/categoryRoute");
 const filterRoute = require("./routes/filterRoute");
 const { notFound } = require('./middlewares/errorHandler');
 
+// CORS settings
 const corsOptions = {
-  origin: 'http://54.169.168.70:4000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
+    origin: ['http://54.169.168.70:3000', 'http://localhost:3000'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
 };
 
-app.use(cors(corsOptions));
+// Middleware
+app.use(cors(corsOptions));  // Use the CORS middleware
 app.use(morgan('dev'));
-app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());  // Use express.json instead of bodyParser.json
+app.use(express.urlencoded({ extended: true }));  // Use express.urlencoded instead of bodyParser.urlencoded
 app.use(cookieParser());
 
-// API Routes
+// Routes
 app.use("/api/auth", authRoute);
 app.use('/api/cart', cartRoute);
 app.use('/api/products', productRoute);
@@ -37,28 +36,21 @@ app.use('/api/categories', categoryRoute);
 app.use('/uploads', express.static('uploads'));
 app.use('/api/filter', filterRoute);
 
-app.get('/', (req, res) => {
-    res.json({ message: "Backend server is running!!" });
-});
-
-// Serve static files from the frontend's build directory
+// Serve frontend static assets
 app.use(express.static(path.join(__dirname, '../frontend/client/build')));
 
-// Catch-all handler: for any request that doesn't match any of the above, send back React's index.html file.
+// Catch-all route to serve the React app
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/client/build/index.html'));
 });
 
+// Error handling middleware
 app.use(notFound);
 
-// Connect to DB and then start the servers
+// Connect to the database and start the server
 const connectDB = require("./database/db");
+connectDB();
 
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running at PORT ${PORT}`);
-    });
-}).catch((error) => {
-    console.error("Failed to connect to DB:", error);
-    process.exit(1);
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT ${PORT}`);
 });
