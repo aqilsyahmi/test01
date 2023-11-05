781b464ff0dc9941e96efa0eb86f25606626f110
aqilsyahmi <aqils1syahmi@gmail.com>
2023-11-05 15:32:03 +0800

    Update functionalTest.js

--- frontend/client/functionalTest.js
+++ frontend/client/functionalTest.js
@@ -7,17 +7,33 @@ const puppeteer = require('puppeteer-core');
   });
 
   const page = await browser.newPage();
-  let maxRetries = 5;
-  for (let i = 0; i < maxRetries; i++) {
-    try {
-      await page.goto('http://18.143.174.103:3000', { timeout: 30000 }); // wait up to 30 seconds
-      break; // exit loop if successful
-    } catch (error) {
-      if (i === maxRetries - 1) {
-        throw error; // if last retry, throw the error
+  
+  // Set an array of possible URLs
+  const possibleURLs = ['http://18.143.174.103:3000', 'https://www.testserver.eloquent-ganguly.cloud/'];
+  let success = false;
+
+  for (let url of possibleURLs) {
+    let maxRetries = 5;
+    for (let i = 0; i < maxRetries; i++) {
+      try {
+        await page.goto(url, { timeout: 30000 }); // wait up to 30 seconds
+        success = true;
+        break; // exit loop if successful
+      } catch (error) {
+        if (i === maxRetries - 1) {
+          console.error(`Failed to navigate to ${url} after ${maxRetries} retries.`);
+        } else {
+          await new Promise(resolve => setTimeout(resolve, 3000)); // wait 3 seconds before retrying
+        }
       }
-      await new Promise(resolve => setTimeout(resolve, 3000)); // wait 3 seconds before retrying
     }
+    if (success) {
+      break; // exit the outer loop if one of the URLs was successful
+    }
+  }
+
+  if (!success) {
+    throw new Error("Failed to navigate to any of the specified URLs.");
   }
 
   // Check for the "Featured Products" heading
