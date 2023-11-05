baaa2bfacd6aee581bdf9aae0c9fd81c06948634
aqilsyahmi <aqils1syahmi@gmail.com>
2023-11-05 15:24:36 +0800

    Update functionalTest.js

--- frontend/client/functionalTest.js
+++ frontend/client/functionalTest.js
@@ -7,10 +7,18 @@ const puppeteer = require('puppeteer-core');
   });
 
   const page = await browser.newPage();
-  await page.goto('http://18.143.174.103:3000'); // adjust this if your app URL is different
-
-  // Wait for 15 seconds
-  await new Promise(resolve => setTimeout(resolve, 15000)); 
+  let maxRetries = 5;
+  for (let i = 0; i < maxRetries; i++) {
+    try {
+      await page.goto('http://18.143.174.103:3000', { timeout: 30000 }); // wait up to 30 seconds
+      break; // exit loop if successful
+    } catch (error) {
+      if (i === maxRetries - 1) {
+        throw error; // if last retry, throw the error
+      }
+      await new Promise(resolve => setTimeout(resolve, 3000)); // wait 3 seconds before retrying
+    }
+  }
 
   // Check for the "Featured Products" heading
   const heading = await page.$x("//*[text()='Featured Products']");
